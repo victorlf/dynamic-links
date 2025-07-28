744537c141c0d682714aff2ad61d74e28b8dc44a
Victor Fonseca <victorlfonseca@gmail.com>
2025-07-28 16:12:16 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -57,7 +57,7 @@ export default async ({ req, res, log }) => {
         .split('{{APP_PACKAGE}}')
         .join(target.appPackage ?? '')
         .split('{{FALLBACK}}')
-        .join(target.fallback ?? target.default ?? '')
+        .join(target.fallback ?? targets.default ?? '')
 
       return res.send(html, 200, {
         'Content-Type': 'text/html; charset=utf-8',
